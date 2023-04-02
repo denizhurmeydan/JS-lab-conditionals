c2a832fe17f535f49886b499c2878093b9768e50
Deniz Hürmeydan <101830721+denizhurmeydan@users.noreply.github.com>
2023-04-02 15:45:25 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+## Activity: Grade Calculator
+
+Create a program that calculates the grade of a student based on their score in a test. The program should assign a score to a variable, then use if-else statements to assign a grade based on the following scale:
+
+- A: 90-100
+- B: 80-89
+- C: 70-79
+- D: 60-69
+- F: below 60
+
+After assigning a grade, the program should display the grade to the user using the `console.log` function.
+
+### Example Output
+
+`Your score is 85, Your grade is B`
+
+
+### Requirements
+
+- The program should assign a score to a variable using an integer data type.
+- The program should use if-else statements to assign a grade based on the score and store it in a variable.
+- The program should display the score and grade to the user using the `console.log` function.
+
+### Bonus
+
+- Refactor the if-else and use Switch instead.
---
+++ index.js
@@ -0,0 +1,44 @@
+const score = 50
+
+
+if ( score>=90 && score<=100 ){
+ console.log("Your score is " + score + " your grade is A")
+}
+else if (score>=80 && score<=89){
+  console.log("Your score is " + score + " your grade is B")
+}
+else if (score>=70 && score<=79){
+  console.log("Your score is " + score + " your grade is C")
+}
+else if (score>=60 && score<=69){
+  console.log("Your score is " + score + " your grade is D")
+}
+else if (score<60) {
+  console.log("Your score is " + score + " your grade is F")
+}
+
+// switch part
+
+// switch(true){
+//   case score>=90 && score<=100:
+//     console.log("Your score is " + score + " your grade is A");
+//     break;
+//       case score>=80 && score<=89:
+//     console.log("Your score is " + score + " your grade is A");
+//     break;
+//       case score>=70 && score<=79:
+//     console.log("Your score is " + score + " your grade is A");
+//     break;
+//       case score>=60 && score<=69:
+//     console.log("Your score is " + score + " your grade is A");
+//     break;
+//       case score<60:
+//     console.log("Your score is " + score + " your grade is A");
+//     break;
+
+//   default:
+//     console.log("undefined");
+// } 
+
+
+
